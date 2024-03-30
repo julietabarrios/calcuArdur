5e0303cb87482a23f80040930fb4c6f94518b1a1
Julieta Barrios <julietabarriosd@gmail.com>
2024-03-30 00:00:17 -0300

    stucture

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Simulador</h1>
+        <a href="https://tienda.ardur.com.ar/">Regresar a la página web de Ardur</a>
       </header>
+      <body>
+        <label for="cars">Choose a car:</label>
+          <select id="cars" name="cars">
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="fiat">Fiat</option>
+            <option value="audi">Audi</option>
+          </select>
+      </body>
     </div>
   );
 }
